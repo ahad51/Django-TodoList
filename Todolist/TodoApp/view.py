e328eed9176fce63f07e7b3b2de6e0b657e45ee5
ahad51 <abdul.ahad@zweidevs.com>
2024-05-25 11:28:43 +0500

    Done with Add to todo list

--- Todolist/TodoApp/view.py
+++ Todolist/TodoApp/view.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Mytodo
+from .forms import TodoForm
 
 def alltodos(request):
-    todos = Mytodo.objects.all()
-    return render(request, 'alltodo.html', {'todos': todos})
+    tasks = Mytodo.objects.all()
+    form=TodoForm
+    if request.method=='POST':
+        form=TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'alltodo.html', {'tasks': tasks , 'form':form})
