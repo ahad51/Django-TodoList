4767ea8883bb1e231328ac60ef1d2e6d686f6450
ahad51 <abdul.ahad@zweidevs.com>
2024-05-31 22:21:18 +0500

    Enhance Code

--- Todolist/TodoApp/urls.py
+++ Todolist/TodoApp/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .view import deleteItem,updateItem,alltodos
 
 
-urlpatterns = [
+urlpatterns = 
+[
     path('', alltodos, name='alltodos'),
-    path('delete_item/<int:pk>/', deleteItem, name='deleteitem'),
+    path('delete_item/<int:pk>/', deleteItem, name='deleteitem')
+    ,
     path('update_item/<int:pk>/', updateItem, name='updateitem')
 ]
