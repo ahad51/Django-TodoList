b73ebe522266f8058dc123aca003151464897597
ahad51 <abdul.ahad@zweidevs.com>
2024-06-05 23:14:05 +0500

    Modify the Controller

--- Todolist/TodoApp/view.py
+++ Todolist/TodoApp/view.py
@@ -24,6 +24,5 @@ def updateItem(request, pk):
         updateForm = TodoForm(request.POST, instance=todo)
         if updateForm.is_valid():
             updateForm.save()
-            return redirect('alltodos')
-              
+            return redirect('alltodos')      
     return render(request, 'updateItem.html', {'todo': todo, 'updateForm': updateForm})
